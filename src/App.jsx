7e1635b8a9815ddcd544c59a4b392445b36a3ed3
smRid <omorsk911@gmail.com>
2025-04-05 12:02:40 +0600

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Coin from './pages/Home/Coin/Coin'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/coin/:coinId' element={<Coin/>}/>
+        </Routes>
+        <Footer/>
     </div>
   )
 }
