f37a0547ef294a8dfc67b336d5c632237f140ce7
smRid <omorsk911@gmail.com>
2025-04-05 12:26:14 +0600

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Coin from './pages/Coin/Coin'
+import Coin from './pages/Home/Coin/Coin'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/coin/:id' element={<Coin/>}/>
-      </Routes>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/coin/:coinId' element={<Coin/>}/>
+        </Routes>
+        <Footer/>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,62 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const NavBar = () => {
+
+  const {setCurrency} = useContext(CoinContext)
+  
+  const currencyHandler = (event)=>{
+    switch (event.target.value){
+      case "usd": {
+        setCurrency({name: "usd", Symbol: "$"});
+        break;
+      }
+      case "eur": {
+        setCurrency({name: "eur", Symbol: "€"});
+        break;
+      }
+      case "inr": {
+        setCurrency({name: "inr", Symbol: "₹"});
+        break;
+      }
+      default : {
+        setCurrency({name: "usd", Symbol: "$"});
+        break;
+      }
+    }
+
+  }
+
+
+
   return (
     <div className='navbar'>
-      <div className="logo-container">
-        <img src={logo} alt="logo" />
-        <span className="logo-text">Cryptoline</span>
-      </div>
-      <ul>
-        <li>Home</li>
-        <li>Features</li>
-        <li>Pricing</li>
-        <li>Blog</li>
-      </ul>
-      <div className="nav-right">
-        <select>
-            <option value="USD"></option>
-            <option value="EUR"></option>
-        </select>
-        <button>Sign Up <img src={arrow_icon} alt="arrow_icon" /></button>
-      </div>
+      <Link to={'/'} className="logo-container">
+      <img src={logo} alt="" className='logo'/>
+      <span className="logo-text">Cryptoline</span>
+      </Link>
+
+        <ul>
+        <Link to={'/'}> <li>Home</li> </Link>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+        </ul>
+        
+        <div className="nav-right">
+            <select onChange={currencyHandler}>
+                <option value="usd">USD</option>
+                <option value="eur">EUR</option>
+                <option value="inr">INR</option>
+            </select>
+            <button>Sign up <img src={arrow_icon} alt=""/></button>
+        </div>      
     </div>
   )
 }
 
-export default Navbar
+export default NavBar
